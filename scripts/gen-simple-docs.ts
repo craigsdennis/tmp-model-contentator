1c608de233ce99b6e612a3dc8859d7e9db877a42
Craig Dennis <craigsdennis@gmail.com>
2024-02-27 21:39:36 -0800

    Adds JSON schemas

--- scripts/gen-simple-docs.ts
+++ scripts/gen-simple-docs.ts
@@ -1,6 +1,8 @@
 import * as YAML from "json-to-pretty-yaml";
 import * as fs from "node:fs";
 import * as path from "node:path";
+import { modelMappings } from "@cloudflare/ai";
+
 
 // pwd in the root of your cloudflare-docs
 const DOCS_ROOT_PATH = process.env.DOCS_ROOT_PATH;
@@ -25,27 +27,49 @@ function taskTypeFromName(taskName) {
   return taskName.toLowerCase().split(" ").join("-");
 }
 
+function getSchemaDefinitions() {
+  const tasks = Object.keys(modelMappings);
+  const schemaDefinitions = {};
+  for (const task of tasks) {
+    try {
+      const AiClass = modelMappings[task].class;
+      const cls = new AiClass();
+      schemaDefinitions[task] = {
+        input: JSON.stringify(cls.schema.input),
+        output: JSON.stringify(cls.schema.output),
+      }
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  return schemaDefinitions;
+}
+
 (async () => {
   const models = await fetchModels();
   console.log(`Found ${models.length} models`);
+  const schemaDefinitions = getSchemaDefinitions();
   // FileName => frontMatter
   const frontMatters = models.reduce((registry, model) => {
+    const taskType = taskTypeFromName(model.task.name);
     const params = {
       model: model,
-      task_type: taskTypeFromName(model.task.name),
+      task_type: taskType,
       model_display_name: model.name.split("/").at(-1),
       layout: "model",
     };
     params["title"] = params.model_display_name;
+    params["json_schema"] = schemaDefinitions[taskType] || "";
     registry[`${params.model_display_name}.md`] = YAML.stringify(params);
     return registry;
   }, {});
-  const taskTypes = new Set<string>(
-    models.map((model) => taskTypeFromName(model.task.name))
-  );
+
   for (const [fileName, frontMatter] of Object.entries(frontMatters)) {
     const filePath = path.join("/tmp", fileName);
-    fs.writeFileSync(`${filePath}`, `---\n${frontMatter}\n---\n\nTODO: JSON Schemas`)
+    fs.writeFileSync(
+      `${filePath}`,
+      `---\n${frontMatter}\n---\n`
+    );
     console.log(`Wrote ${filePath}`);
   }
 })();
